9043e03d665fa32af0f3353d995bb2d0e6e0842d
FlorianXXIV <floriankokotz@gmail.com>
2023-01-24 10:24:31 +0100

    Hinzufügen des Einführungstextes der Matrixrechnung
    ver 0.3.0 dev 0.3.0.2 FlorianXXIV

--- Programm/Matrixrechnung.java
+++ Programm/Matrixrechnung.java
@@ -7,6 +7,10 @@ public class Matrixrechnung {
     public void matrixrechnung(int rounds){
         System.out.println("""
                 Bei der Matrixrechnung kannst du zwischen verschiedenen Operationen Waehlen.
+                0-Zufaellige Matrixoperationen (Add,Mult)
+                1-Gauss Transformation
+                2-Loesen Linearer Gleichungssysteme
+                3-Determinanten Berechnen
                 """);
     }
 }
